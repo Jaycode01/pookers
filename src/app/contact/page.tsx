f7792e77bb74a5d87a553015de86b0bb4dd6218f
Jaycode01 <josephlamidi01@gmail.com>
2025-07-20 21:56:44 +0100

    fisihed the contact us page - with full responsiveness

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/footer";
 import { Facebook, House, Instagram, Mail, Phone, Twitter } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
@@ -6,7 +7,7 @@ import React from "react";
 export default function Contact() {
   return (
     <>
-      <section className="relative h-[250px] md:h-[400px]">
+      <section className="relative h-[200px] md:h-[400px]">
         <Image
           src="/images/photo_bg_about.jpg"
           alt="about page top bg image"
@@ -25,11 +26,11 @@ export default function Contact() {
           </h1>
         </div>
       </section>
-      <section className="text-gray-600 mt-20 px-[5%] flex flex-row gap-10">
-        <div className="w-[65%]">
-          <h1 className="text-2xl uppercase">send us a message</h1>
+      <section className="text-gray-600 mt-20 px-[5%] flex flex-col md:flex-row gap-10">
+        <div className="w-full md:w-[65%]">
+          <h1 className="text-xl md:text-2xl uppercase">send us a message</h1>
           <form className="flex flex-col gap-5 mt-5">
-            <div className="grid grid-cols-3 gap-10">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-5 md:gap-10">
               <input
                 type="text"
                 placeholder="Your Name"
@@ -48,13 +49,18 @@ export default function Contact() {
             </div>
             <textarea
               placeholder="Your Message"
-              className="border border-gray-500 text-inherit p-3 text-sm outline-none h-[250px]"
+              className="border border-gray-500 text-inherit p-3 text-sm outline-none h-[150px] md:h-[250px]"
             />
-            <button type="submit" className=""></button>
+            <button
+              type="submit"
+              className="text-left bg-orange-500 w-fit px-7  py-3 text-white text-sm"
+            >
+              Send Message
+            </button>
           </form>
         </div>
         <div className="">
-          <h1 className="text-2xl uppercase">contact info</h1>
+          <h1 className="text-xl md:text-2xl uppercase">contact info</h1>
           <ul className="flex flex-col gap-2.5 mt-5">
             <li className="flex flex-row gap-1.5 items-center">
               <House size={18} />
@@ -101,6 +107,19 @@ export default function Contact() {
           </ul>
         </div>
       </section>
+      <section className="w-full mt-10 md:mt-20">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.10123624524!2d3.376178175847342!3d6.50886832333144!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8c58aef0ab5f%3A0x35d0d9dce060b34e!2s5%20Birrel%20Ave%2C%20Onike%2C%20Lagos%20101245%2C%20Lagos!5e0!3m2!1sen!2sng!4v1753044670209!5m2!1sen!2sng"
+          style={{ border: 0 }}
+          allowFullScreen
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+          className="w-full h-[250px] md:h-[500px]"
+        ></iframe>
+      </section>
+      <footer className="mt-2">
+        <Footer />
+      </footer>
     </>
   );
 }
