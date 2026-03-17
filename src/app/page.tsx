c16223ce48eb8726edf9a243f478bd114c228f87
Jaycode01 <josephlamidi01@gmail.com>
2026-03-17 14:06:39 +0100

    feat(analytics): add google analytics to google

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,6 +64,22 @@ export default function Home() {
             }),
           }}
         />
+
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-TZN89V2CP7`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || []; 
+              function gtag()
+              {dataLayer push(arguments);}
+                gtag('js', new Date())'
+                gtag('config', 'G-TZN89V2CP7')
+            `,
+          }}
+        />
       </Head>
       <>
         <Hero />
